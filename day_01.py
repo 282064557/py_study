240fec3c6fef1b35f32e8726e4fa09d109f9e06e
栗子先生_lily <282064557@qq.com>
2020-10-10 14:01:35 +0800

    修改帮助文档

--- day_01.py
+++ day_01.py
@@ -10,18 +10,18 @@ else:
 
 #多行语句
 # Python 通常是一行写完一条语句，但如果语句很长，我们可以使用反斜杠(\)来实现多行语句，例如：
-total = item_one + \
-        item_two + \
-        item_three
+#total = item_one + \
+  #      item_two + \
+  #      item_three
 # 在 [], {}, 或 () 中的多行语句，不需要使用反斜杠(\)，例如：
 total = ['item_one','item_two','item_three'] 
 
 
 #数字类型
 # python中数字有四种类型：整数、布尔型、浮点数和复数。
-int (整数), 如 1, 只有一种整数类型 int，表示为长整型，没有 python2 中的 Long。
-bool (布尔), 如 True。
-float (浮点数), 如 1.23、3E-2
-complex (复数), 如 1 + 2j、 1.1 + 2.2j
+#int (整数), 如 1, 只有一种整数类型 int，表示为长整型，没有 python2 中的 Long。
+#bool (布尔), 如 True。
+#float (浮点数), 如 1.23、3E-2
+#complex (复数), 如 1 + 2j、 1.1 + 2.2j
 
 
