51401b2f2399aa6704a90c21279efa82c0d02447
栗子先生_lily <282064557@qq.com>
2020-10-12 12:14:47 +0800

    fbjdhsk

--- day_02.py
+++ day_02.py
@@ -1,3 +1,29 @@
+name = "egon"
+print(name)
+print("-----------这是什么？----------------")
+print("牛逼！")
+print("1123123")
 import sys
-print("你是谁啊？")
-print("你是谁")
+
+# X = 10
+# print(X)
+
+
+# x = y = 10
+# print(x)
+# print(y)
+
+
+x = 10
+y = x
+z = y
+del x    #解除变量名x与10的绑定关系
+print(x)
+import sys
+del x
+print("jbj b")
+
+
+
+
+
